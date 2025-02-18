8049db11d9a47d01c4a1a2b938b11161fafe1baf
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-02-18 21:04:41 +0100

    Update comments_count

--- posts/views.py
+++ posts/views.py
@@ -49,5 +49,5 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
         likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
+        comments_count=Count('comments', distinct=True)
     ).order_by('-created_at')
